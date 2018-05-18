ea2f2d1355a3eb563a24e6e0e23a1aab86b1d49a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-18 18:26:09 +0100

    Update setavatar.js

--- ibotv1-master/commands/setavatar.js
+++ ibotv1-master/commands/setavatar.js
@@ -1,6 +1,6 @@
 
 exports.run = (Discord, client, message, args) => {
-if (message.member.hasPermission("ADMINISTRATOR") {
+if(message.member.hasPermission("ADMINISTRATOR")) {
   const sayMessage = args.join(" ");
   client.user.setAvatar(sayMessage);
   }
