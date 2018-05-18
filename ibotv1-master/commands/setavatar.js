4c44285f8b05a45fa795e134673e577118f86c8e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-18 18:18:17 +0100

    Update setavatar.js

--- ibotv1-master/commands/setavatar.js
+++ ibotv1-master/commands/setavatar.js
@@ -3,4 +3,4 @@ exports.run = (Discord, client, message, args) => {
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Only admins can edit premium bot settings.!");
 const sayMessage = args.join(" ");
 client.user.setAvatar(sayMessage);
-  
+}
