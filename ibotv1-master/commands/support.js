462061966ffebf2e635ee0e2d0d908a1c9b09e8f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-18 20:50:01 +0100

    Update support.js

--- ibotv1-master/commands/support.js
+++ ibotv1-master/commands/support.js
@@ -1,9 +1,9 @@
-exports.run = (Discord, client, message, args) => {
+exports.run = async (Discord, client, message, args) => {
     let myRole = message.guild.roles.find("name", "Supporter");
     if(message.channel.id !== "447117133952974859") return message.channel.send("This command can only be executed in the `Alert eSports roles` channel!");
     if(message.member.roles.has(myRole.id)) return message.channel.send("You already have the role!");
         message.reply("You've been given the support role!")
         let member = message.author;
-        member.addRole(myRole).catch(console.error);
+        await (message.author.addRole(myRole.id)).catch(console.error)
     
 }
