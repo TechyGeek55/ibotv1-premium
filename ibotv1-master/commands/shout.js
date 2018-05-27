3467e36d4ed3bfbf9e29551653ac4f4d881f27b2
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 16:57:38 +0100

    Update shout.js

--- ibotv1-master/commands/shout.js
+++ ibotv1-master/commands/shout.js
@@ -4,12 +4,13 @@
 var roblox = require('noblox.js');
 
 exports.run = (Discord, client, message, args) => {
+if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
 roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
 
 }).catch(() => {console.log("Failed to login.");});
 
 setTimeout(function() {
-if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
+
 var jar = roblox.options.jar;	
 	
 var groupId = process.env.group;
