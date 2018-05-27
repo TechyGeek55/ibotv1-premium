ac4ea87c86bb80c66d62e5cab79aa12f25003951
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 09:05:59 +0100

    Update promote.js

--- ibotv1-master/commands/promote.js
+++ ibotv1-master/commands/promote.js
@@ -3,7 +3,7 @@ var roblox = require('noblox.js');
 exports.run = (Discord, client, message, args) => {
 if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
 	
-	
+let staffc = message.guild.channels.find("name", "logs")	
 var groupId = process.env.group;
 var maximumRank = process.env.rank;
 
@@ -27,6 +27,11 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 						roblox.promote(groupId, id)
 						.then(function(roles){
 							message.channel.send(`Promoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+							const embed = new Discord.RichEmbed()
+							    .setColor(0x8cff00)
+							    .setTimestamp()
+							    .setDescription(`**Action:** Demote\n**Target:** ${username}\n**User:** ${message.author.tag}`);
+							staffc.send({embed});
 						}).catch(function(err){
 							message.channel.send("Failed to promote.")
 						});
