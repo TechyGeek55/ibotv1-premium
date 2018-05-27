94be6b354fc35d563086e0310a28bdb328279608
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 09:30:39 +0100

    Update shout.js

--- ibotv1-master/commands/shout.js
+++ ibotv1-master/commands/shout.js
@@ -18,7 +18,7 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 
     	var username = args[0]
     	if (username){
-    		let testestse = 2
+    		message.channel.send("You have correct permissions.")
 			.then(function(id){
 				message.channel.send("Shouting....")
 			        
