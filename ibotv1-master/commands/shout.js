ec9f5071b38a99725e47ce7835289c25f2ed344d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 09:29:25 +0100

    Update shout.js

--- ibotv1-master/commands/shout.js
+++ ibotv1-master/commands/shout.js
@@ -1,38 +1,49 @@
+
+
+
 var roblox = require('noblox.js');
 
 exports.run = (Discord, client, message, args) => {
 if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
 	
-let staffc = message.guild.channels.find("name", "logs")	
+	
 var groupId = process.env.group;
-var maximumRank = process.env.rank;
-let rank2 = args[1];
+var minimumRank = 1;
+let staffc = message.guild.channels.find("name", "logs")
+var message = args[0];
 
 roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
 
 }).catch(() => {console.log("Failed to login.");});
 
+    	var username = args[0]
+    	if (username){
+    		let testestse = 2
+			.then(function(id){
+				message.channel.send("Shouting....")
+			        
+				.then(function(shout){
 					
-						roblox.shout(groupId, args[0])
+						roblox.shout(groupId, message)
 						.then(function(roles){
-							message.channel.send(`Shouted!`)
+							message.channel.send(`Shouted.`)
 							const embed = new Discord.RichEmbed()
 							    .setColor(0x8cff00)
 							    .setTimestamp()
-							    .setDescription(`**Action:** Shout\n**Shouted:** ${rank2}\n**User:** ${message.author.tag}`);
+							    .setDescription(`**Action:** Shout\n**Shouted:** ${message}\n**User:** ${message.author.tag}`);
 							staffc.send({embed});
 						}).catch(function(err){
-							message.channel.send("Failed to shout.")
+							message.channel.send("Failed to Shout.")
 						});
 					
 				}).catch(function(err){
-					message.channel.send("Couldn't shout.")
+					message.channel.send("Failed to Shout.")
 				});
 			}).catch(function(err){ 
-				message.channel.send(`Couldn't shout.`)
+				message.channel.send(`Failed to Shout.`)
 			});
     	} else {
-    		message.channel.send("Couldn't shout.")
-    	
+    		message.channel.send("Failed to Shout.")
+    	}
     	return;
 }
