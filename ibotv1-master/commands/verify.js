1a41c8a41ba744968a9880866428fa572c1c2e81
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 16:24:27 +0100

    Create verify.js

---
+++ ibotv1-master/commands/verify.js
@@ -0,0 +1,58 @@
+var roblox = require('noblox.js');
+
+exports.run = (Discord, client, message, args) => {
+
+roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
+
+}).catch(() => {console.log("Failed to login.");});
+
+let args = message.content.split(" ").slice(1);
+let amount = args[0];
+ 
+let godRole = message.guild.roles.find("name", "Verified");
+if (message.member.roles.has(godRole.id)) {
+  return message.reply("Already verified, please contact an administrator if you're having a problem.")
+}
+    message.reply("please check your ROBLOX.com messages. We've messaged you with a verification code, follow instructions found in the message. **Change your settings so all users can message, you may change your settings back once verified.**  \nCopy and paste the specific code that was sent to you.")
+ 
+    var code = [
+      "asdlkasdlkjasdlkj&2918347",
+      "faslkdasldoij$28",
+      "sfdklsdflksdjflskdfjl%",
+      "sdlkfjsdlkfjsdlfkjsdlfkjsdflkjqw123123123",
+      "asldkfasdlkfj*@#(@3812391238)",
+      "zlkjxvlkzjxcvlkjzxcvlk2u34e902183102938",
+      "sdflksdflksdf12812392389",
+      "asdlkasldasdaksdjvxbcvxcvi128u!!!",
+      "asdlfkjasdflkjasdflkjasdfuq30984q023948;;213123",
+      "vjksdanvzxcviewr839"
+    ]
+    let zz = code[Math.floor(Math.random()*code.length)]
+    rbx.login(options)
+        .then(function (info) {
+          rbx.message(args[0], "iBot Verification", "Dear user, \nA user has requested for you to verify with iBot Roblox. If you did not submit this verification then please ignore this message.\n \nHowever, if you did send the message please copy and paste the specific code below onto our discord. \n" + "\n" + zz + "\n" + "\nKind regards, \niBot Team", jar)
+ 
+      })
+ 
+ 
+      .then(() => {
+        message.channel.awaitMessages(response => code.includes(response.content), {
+          max: 1,
+          time: 60000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+          message.delete()
+          let member = message.author
+  let guild = member.guild;
+  message.member.setNickname(args[1])
+          message.member.addRole(message.guild.roles.find('name', 'Verified')).catch(console.error);
+          message.author.sendMessage("You've been officially verified!");
+        })
+ 
+          .catch((c) => {
+            console.log(c)
+            message.channel.sendMessage('Please try again later...');
+          });
+    })
+}
