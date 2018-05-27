6eac81e8b46137eea1f877dde4759da5d2d7d2bc
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 16:41:54 +0100

    Update verify.js

--- ibotv1-master/commands/verify.js
+++ ibotv1-master/commands/verify.js
@@ -27,7 +27,7 @@ if (message.member.roles.has(godRole.id)) {
     let zz = code[Math.floor(Math.random()*code.length)]
     rbx.login({username: process.env.username, password: process.env.password})
         .then(function (info) {
-          rbx.message("AaronnSmh", "iBot Verification", "Please post the following code into the discord server.\n" + "\n" + zz + "\n" + "\nKind regards, \niBot Team", jar)
+          rbx.message("AaronnSmh", "iBot Verification", "Post the code in the channel.\n" + "\n" + zz + "\n" + "\nKind regards, \niBot Team", jar)
  
       })
  
