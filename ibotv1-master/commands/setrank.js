b2769214ce6d393e76534b8d276da570e456d13a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-27 15:33:18 +0100

    Update setrank.js

--- ibotv1-master/commands/setrank.js
+++ ibotv1-master/commands/setrank.js
@@ -1,12 +1,11 @@
 var roblox = require('noblox.js');
 
-exports.run = (Discord, client, message, args) => {
-if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
-	
-let staffc = message.guild.channels.find("name", "logs")	
-var groupId = process.env.group;
-var maximumRank = process.env.rank;
-let rank2 = args[1];
+exports.run = (Discord, client, message, args) => {;
+let allowedRole = message.guild.roles.find("name", "Bot Ranker");	
+if(!message.member.roles.has(allowedRole.id)) return message.channel.send("No can do pal! Only users with the `Bot Ranker` role can run this command.");
+var groupId = 2633056;
+var maximumRank = 6;
+let staffc = message.guild.channels.find("name", "promotion-logs") 
 
 roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
 
@@ -14,25 +13,27 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 
     	var username = args[0]
     	if (username){
-    		message.channel.send(`Checking ROBLOX for ${username}`)
     		roblox.getIdFromUsername(username)
 			.then(function(id){
 				roblox.getRankInGroup(groupId, id)
 			        
 				.then(function(rank){
-					
-						roblox.setRank(groupId, id, rank2)
+					if(maximumRank <= rank){
+						console.log("Command running")
+					} else {
+						roblox.setRank(groupId, id, args[1])
+						
 						.then(function(roles){
-							message.channel.send(`Ranked from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+							message.channel.send("Ranked from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
 							const embed = new Discord.RichEmbed()
 							    .setColor(0x8cff00)
 							    .setTimestamp()
-							    .setDescription(`**Action:** Rank\n**Target:** ${username}\n**User:** ${message.author.tag}`);
+							    .setDescription(`**Action:** Rank\n**Target:** ${username}\n**User:** ${message.author.tag}\n**Old Rank:** ${roles.oldRole.Name}\n**New Rank:** ${roles.newRole.Name}`);
 							staffc.send({embed});
 						}).catch(function(err){
 							message.channel.send("Failed to rank.")
 						});
-					
+					}
 				}).catch(function(err){
 					message.channel.send("Couldn't get them in the group.")
 				});
