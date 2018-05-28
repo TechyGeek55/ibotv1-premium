d434dcbd77810efe44f4aa40442a615bde4582a9
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 08:17:23 +0100

    Update demote.js

--- ibotv1-master/commands/demote.js
+++ ibotv1-master/commands/demote.js
@@ -1,12 +1,10 @@
-var roblox = require('noblox.js');
+ar roblox = require('noblox.js');
 
-exports.run = (Discord, client, message, args) => {
-if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
-	
-	
-var groupId = process.env.group;
-var minimumRank = 1;
-let staffc = message.guild.channels.find("name", "logs")
+exports.run = (Discord, client, message, args) => {;
+if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You need the MANAGE_ROLES permission to run this comamnd!")
+var groupId = 937709;
+var maximumRank = 255;
+let staffc = message.guild.channels.find("name", "logs") 
 
 roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
 
@@ -14,25 +12,27 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 
     	var username = args[0]
     	if (username){
-    		message.channel.send(`Checking ROBLOX for ${username}`)
     		roblox.getIdFromUsername(username)
 			.then(function(id){
 				roblox.getRankInGroup(groupId, id)
 			        
 				.then(function(rank){
-					
+					if(maximumRank = 0){
+						console.log("test")
+					} else {
 						roblox.demote(groupId, id)
+						
 						.then(function(roles){
-							message.channel.send(`Demoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+							message.channel.send(`Demoted user ${username} from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
 							const embed = new Discord.RichEmbed()
 							    .setColor(0x8cff00)
 							    .setTimestamp()
-							    .setDescription(`**Action:** Demote\n**Target:** ${username}\n**User:** ${message.author.tag}`);
+							    .setDescription(`**Action:** Demote\n**Target:** ${username}\n**User:** ${message.author.tag}\n**Old Rank:** ${roles.oldRole.Name}\n**New Rank:** ${roles.newRole.Name}`);
 							staffc.send({embed});
 						}).catch(function(err){
 							message.channel.send("Failed to demote.")
 						});
-					
+					}
 				}).catch(function(err){
 					message.channel.send("Couldn't get them in the group.")
 				});
