3b15c006bd688d88cdfa38238ee3538b66f419c1
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-09 18:55:37 +0100

    Update shout.js

--- ibotv1-master/commands/shout.js
+++ ibotv1-master/commands/shout.js
@@ -8,16 +8,14 @@ if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("
 roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
 
 
-var jar = roblox.options.jar;	
 	
 var groupId = process.env.group;
 var minimumRank = 1;
 let staffc = message.guild.channels.find("name", "logs")
 var message = args[0];
 let shouter = args.slice(0).join(" ");
-
-roblox.shout(groupId, shouter, jar)
-}).catch(() => {console.log("Failed to login.");});
+roblox.shout({group: groupId, message: shouter})
+}).catch((err) => {console.log("Failed to login. " + err);});
 
 
 
