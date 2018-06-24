750fc9f95b5c9f21e37722a3b0aed6107061856e
Nickanda <songyang20092009@gmail.com>
2018-06-24 13:49:43 -0400

    Update fire.js

--- ibotv1-master/commands/fire.js
+++ ibotv1-master/commands/fire.js
@@ -1,45 +1,48 @@
 var roblox = require('noblox.js');
+const db = require('quick.db')
+exports.run = async (Discord, client, message, args) => {
 
-exports.run = (Discord, client, message, args) => {
-if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
-	
-	
-var groupId = process.env.group;
-let staffc = message.guild.channels.find("name", "logs")
-let firerank = 1;
-roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
+  if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
+  let pw = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.password' });
+  let un = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.username' });
+  let gid = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.groupid' });
+  let mr = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.maxrank' });
+  let staffc = message.guild.channels.find("name", "logs")	
+  var groupId = gid; //replace with stored stuff from earlier
+  var maximumRank = mr; //replace with stored stuff from earlier
+  
+  roblox.login({username: un, password: pw}).catch(() => {console.log("Failed to login.");});
 
-}).catch(() => {console.log("Failed to login.");});
-
-    	var username = args[0]
-    	if (username){
-    		message.channel.send(`Checking ROBLOX for ${username}`)
-    		roblox.getIdFromUsername(username)
-			.then(function(id){
-				roblox.getRankInGroup(groupId, id)
-			        
-				.then(function(rank){
-					
-						roblox.setRank(groupId, id, firerank)
-						.then(function(roles){
-							message.channel.send(`Fired from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
-							const embed = new Discord.RichEmbed()
-							    .setColor(0x8cff00)
-							    .setTimestamp()
-							    .setDescription(`**Action:** Fire\n**Target:** ${username}\n**User:** ${message.author.tag}`);
-							staffc.send({embed});
-						}).catch(function(err){
-							message.channel.send("Failed to fire.")
-						});
-					
-				}).catch(function(err){
-					message.channel.send("Couldn't get them in the group.")
-				});
-			}).catch(function(err){ 
-				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+  var username = args[0]
+  if (username) {
+    roblox.getIdFromUsername(username)
+		.then(function(id) {
+			roblox.getRankInGroup(groupId, id)
+			.then(async function(rank) {
+        let oldrole = await roblox.getRankNameInGroup(groupId, id)
+				if(maximumRank <= rank) {
+					message.channel.send(`${id} is rank ${rank} and not promotable.`)
+				} else {
+					roblox.setRank(groupId, id, 1)
+		      .then(function(roles) {
+						message.channel.send(`Fired user ${username} from ${oldrole}`)
+						const embed = new Discord.RichEmbed()
+							.setColor(0x8cff00)
+							.setTimestamp()
+							.setDescription(`**Action:** Fire\n**Target:** ${username}\n**User:** ${message.author.tag}\n**Old Rank:** ${oldrole}\n**New Rank:** ${roles.newRole.Name}`);
+						staffc.send({embed});
+					}).catch(function(err) {
+						message.channel.send("Failed to fire.")
+					});
+				}
+			}).catch(function(err) {
+				message.channel.send("Couldn't get them in the group.")
 			});
-    	} else {
-    		message.channel.send("Please enter a username.")
-    	}
-    	return;
+		}).catch(function(err) { 
+			message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+		});
+  } else {
+    message.channel.send("Please enter a username.")
+  }
+  return;
 }
