d3c5818bda360b1c3f91d7d73e5e1b96b8220b81
Nickanda <songyang20092009@gmail.com>
2018-06-24 14:21:16 -0400

    Update shout.js

--- ibotv1-master/commands/shout.js
+++ ibotv1-master/commands/shout.js
@@ -1,22 +1,32 @@
-
-
-
 var roblox = require('noblox.js');
+const db = require('quick.db')
+exports.run = async (Discord, client, message, args) => {
 
-exports.run = (Discord, client, message, args) => {
-if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
-roblox.login({username: process.env.username, password: process.env.password}).then((success) => {
+  if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("No can do pal!, MANAGE_ROLES is needed.");
+  let pw = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.password' });
+  let un = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.username' });
+  let gid = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.groupid' });
+  let mr = await db.fetch(`RobloxProfile_${message.guild.id}`, { target: '.maxrank' });
+  let staffc = message.guild.channels.find("name", "logs")	
+  var groupId = gid; //replace with stored stuff from earlier
+  var maximumRank = mr; //replace with stored stuff from earlier
+  
+  roblox.login({username: un, password: pw}).catch(() => {console.log("Failed to login.");});
 
-
-	
-var groupId = process.env.group;
-var minimumRank = 1;
-let staffc = message.guild.channels.find("name", "logs")
-var message = args[0];
-let shouter = args.slice(0).join(" ");
-roblox.shout({group: groupId, message: shouter})
-}).catch((err) => {console.log("Failed to login. " + err);});
-
-
-
+  var shoutmessage = args.join(' ');
+  if(shoutmessage.length > 0) {
+    roblox.shout(groupId, shoutmessage).then(() => {
+      message.channel.send('Group shout posted')
+      const embed = new Discord.RichEmbed()
+        .setColor(0x8cff00)
+        .setTimestamp()
+        .setDescription(`**Action:** Group Shout\n**Shout Message:** ${shoutmessage}`);
+      staffc.send({embed})
+    }).catch(e => {
+      message.channel.send('Failed to shout.')
+    });
+  } else {
+    message.channel.send('Please enter a message to shout')
+  }
+  return;
 }
